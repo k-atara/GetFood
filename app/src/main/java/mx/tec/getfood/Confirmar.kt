384118a000923492437b833fe1123f99e3ac7601
k-atara <A01423052@itesm.mx>
2020-10-08 17:14:51 -0500

    Estilo de boton y Activity de confirmar

---
+++ app/src/main/java/mx/tec/getfood/Confirmar.kt
@@ -0,0 +1,11 @@
+package mx.tec.getfood
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Confirmar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_confirmar)
+    }
+}
