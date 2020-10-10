ba1635dcd12f485abb7faffc9437a8969f2b7fac
MARIOFR33man <64052458+MARIOFR33man@users.noreply.github.com>
2020-10-10 15:47:57 -0500

    login y registro

---
+++ app/src/main/java/mx/tec/getfood/ui/Registro.kt
@@ -0,0 +1,12 @@
+package mx.tec.getfood.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import mx.tec.getfood.R
+
+class Registro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro)
+    }
+}
