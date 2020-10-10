30d4233afa014074f233ae730751ed56d03c41a5
MARIOFR33man <64052458+MARIOFR33man@users.noreply.github.com>
2020-10-10 14:36:15 -0500

    Agregue funcionalidad

--- app/src/main/java/mx/tec/getfood/Confirmar.kt
+++ app/src/main/java/mx/tec/getfood/Confirmar.kt
@@ -1,11 +1,15 @@
 package mx.tec.getfood
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Confirmar : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_confirmar)
+
+
     }
 }
