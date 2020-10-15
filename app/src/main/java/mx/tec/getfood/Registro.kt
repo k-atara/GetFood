d87dfb69c777ffe5a78bc3f9b0193f67f26eefdf
MARIOFR33man <64052458+MARIOFR33man@users.noreply.github.com>
2020-10-15 14:09:32 -0500

    hasta aqui llego mi compu

---
+++ app/src/main/java/mx/tec/getfood/Registro.kt
@@ -0,0 +1,29 @@
+package mx.tec.getfood
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import mx.tec.getfood.R
+
+class Registro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro)
+
+        val b0=findViewById<Button>(R.id.btn_registro)
+        b0.setOnClickListener {
+            val i = Intent(this@Registro, LogIn::class.java)
+
+            startActivity(i)
+        }
+
+        val b1=findViewById<Button>(R.id.btm_cancelar)
+        b0.setOnClickListener {
+            val i = Intent(this@Registro, MainActivity::class.java)
+
+            startActivity(i)
+        }
+
+    }
+}
