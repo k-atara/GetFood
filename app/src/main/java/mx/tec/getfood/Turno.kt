898a25cd2554e47370c1a4a350781a4fe8248fe4
JuanpaHdez <49568104+JuanpaHdez@users.noreply.github.com>
2020-10-15 17:32:50 -0500

    Layouts
    Puse los 3 layouts sin ninguna funcionalidad aún

---
+++ app/src/main/java/mx/tec/getfood/Turno.kt
@@ -0,0 +1,12 @@
+package mx.tec.getfood
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Turno : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_turno)
+
+    }
+}
