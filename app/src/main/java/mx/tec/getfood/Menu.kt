099f4d1e332fa7dff2dc43d4f5cc10c88c73d1d0
k-atara <A01423052@itesm.mx>
2020-10-28 22:48:25 -0600

    navigation bar y menu hamburguesa

--- app/src/main/java/mx/tec/getfood/Menu.kt
+++ app/src/main/java/mx/tec/getfood/Menu.kt
@@ -34,11 +34,6 @@ class Menu : AppCompatActivity() {
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-        val fab: FloatingActionButton = findViewById(R.id.fab)
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
@@ -49,9 +44,18 @@ class Menu : AppCompatActivity() {
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_turno
             ), drawerLayout
         )
+
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
+
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.btn_nav)
+
+        val navController1 = findNavController(R.id.buton_nav)
+
+        bottomNavigation.setupWithNavController(navController1)
+
+        /*
         platilloFragment = PlatilloFragment()
         supportFragmentManager
             .beginTransaction()
@@ -88,7 +92,7 @@ class Menu : AppCompatActivity() {
                 }
             }
             true
-        }
+        }*/
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
