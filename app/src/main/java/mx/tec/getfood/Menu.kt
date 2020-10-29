bac967c396784e443694ba308eaf6f8d2971fda3
k-atara <A01423052@itesm.mx>
2020-10-29 01:20:01 -0600

    menu

--- app/src/main/java/mx/tec/getfood/Menu.kt
+++ app/src/main/java/mx/tec/getfood/Menu.kt
@@ -6,17 +6,16 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.FragmentTransaction
+import androidx.navigation.NavController
+import androidx.navigation.ui.*
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.content_main.*
 import mx.tec.getfood.elemento.adapter.CustomAdapter
 import mx.tec.getfood.elemento.model.Elemento
 import mx.tec.getfood.ui.home.HomeFragment
@@ -24,9 +23,7 @@ import mx.tec.getfood.ui.home.HomeFragment
 class Menu : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
-    lateinit var  platilloFragment: PlatilloFragment
-    lateinit var bebidaFragment: BebidaFragment
-    lateinit var postreFragment: PostreFragment
+    lateinit var navController:NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,74 +32,35 @@ class Menu : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
-        val navView: NavigationView = findViewById(R.id.nav_view)
-        val navController = findNavController(R.id.nav_host_fragment)
+        //val navView: NavigationView = findViewById(R.id.navigation_view)
+        //val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_turno
-            ), drawerLayout
-        )
 
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
-
-
+        //Bottom Navigation
         val bottomNavigation: BottomNavigationView = findViewById(R.id.btn_nav)
+        navController = findNavController(R.id.buton_nav)
+        bottomNavigation.setupWithNavController(navController)
 
-        val navController1 = findNavController(R.id.buton_nav)
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+        NavigationUI.setupActionBarWithNavController(this,navController,drawerLayout)
+        //setupActionBarWithNavController(navController, appBarConfiguration)
+        //navView.setupWithNavController(navController)
 
-        bottomNavigation.setupWithNavController(navController1)
+        //Drawer
+        NavigationUI.setupWithNavController(navigation_view, navController)
 
-        /*
-        platilloFragment = PlatilloFragment()
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.frame_layout,platilloFragment)
-            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-            .commit()
-
-        val bottomNavigation:BottomNavigationView = findViewById(R.id.btn_nav)
-        bottomNavigation.setOnNavigationItemSelectedListener { item ->
-            when (item.itemId){
-                R.id.platoFuerte -> {
-                    platilloFragment = PlatilloFragment()
-                    supportFragmentManager
-                        .beginTransaction()
-                        .replace(R.id.frame_layout,platilloFragment)
-                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-                        .commit()
-                }
-                R.id.bebidas -> {
-                    bebidaFragment = BebidaFragment()
-                    supportFragmentManager
-                        .beginTransaction()
-                        .replace(R.id.frame_layout,bebidaFragment)
-                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-                        .commit()
-                }
-                R.id.postres -> {
-                    postreFragment = PostreFragment()
-                    supportFragmentManager
-                        .beginTransaction()
-                        .replace(R.id.frame_layout,postreFragment)
-                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-                        .commit()
-                }
-            }
-            true
-        }*/
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+    /*override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu, menu)
         return true
-    }
+    }*/
 
     override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment)
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+        //val navController = findNavController(R.id.nav_host_fragment)
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+        //return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
