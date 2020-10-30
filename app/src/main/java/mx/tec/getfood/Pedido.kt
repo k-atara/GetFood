037ce7dfedd3ec4709f99d25cca8d499cba764da
MARIOFR33man <64052458+MARIOFR33man@users.noreply.github.com>
2020-10-29 20:45:34 -0600

    NosepudoElFragment

--- app/src/main/java/mx/tec/getfood/Pedido.kt
+++ app/src/main/java/mx/tec/getfood/Pedido.kt
@@ -1,11 +1,22 @@
 package mx.tec.getfood
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import mx.tec.getfood.ui.turno.Turno
 
 class Pedido : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pedido)
+
+        val b0=findViewById<Button>(R.id.btn_turno)
+        b0.setOnClickListener {
+            val i = Intent(this@Pedido, Menu::class.java)
+
+            startActivity(i)
+        }
+
     }
 }
