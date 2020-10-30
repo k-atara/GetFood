5200370e1982c9e114df3f3b4020a8c62c9b4045
k-atara <A01423052@itesm.mx>
2020-10-30 08:02:43 -0600

    btn regresar de orden

--- app/src/main/java/mx/tec/getfood/Pedido.kt
+++ app/src/main/java/mx/tec/getfood/Pedido.kt
@@ -4,14 +4,13 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import mx.tec.getfood.ui.turno.Turno
 
 class Pedido : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pedido)
 
-        val b0=findViewById<Button>(R.id.btn_turno)
+        val b0=findViewById<Button>(R.id.btnOrdenar)
         b0.setOnClickListener {
             val i = Intent(this@Pedido, Menu::class.java)
 
