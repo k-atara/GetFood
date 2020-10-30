b16a3e5af89f525bcdd7060bd500f57ce701b6d9
k-atara <A01423052@itesm.mx>
2020-10-29 22:46:56 -0600

    registro de usuarios

--- app/src/main/java/mx/tec/getfood/Registro.kt
+++ app/src/main/java/mx/tec/getfood/Registro.kt
@@ -3,19 +3,36 @@ package mx.tec.getfood
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.activity_registro.*
 import mx.tec.getfood.R
+import org.json.JSONObject
 
 class Registro : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro)
 
+
+
+
         val b0=findViewById<Button>(R.id.btn_registro)
         b0.setOnClickListener {
-            val i = Intent(this@Registro, LogIn::class.java)
+            val nombre = edtNombre.text.toString()
+            val correo = edtCorreo.text.toString()
+            val usuario = edtUsuario.text.toString()
+            var contrasena = edtContrasena.text.toString()
 
+            addPersona(nombre, correo, usuario, contrasena)
+
+            val i = Intent(this@Registro, LogIn::class.java)
             startActivity(i)
+
         }
 
         val b1=findViewById<Button>(R.id.btm_cancelar)
@@ -25,5 +42,27 @@ class Registro : AppCompatActivity() {
             startActivity(i)
         }
 
+    }
+    fun addPersona(nombre:String, correo:String, usuario:String, contrasena:String){
+
+        var json = JSONObject()
+        json.put("nombre", nombre)
+        json.put("correo", correo)
+        json.put("nickname", usuario)
+        json.put("password", contrasena)
+
+        var resp = false;
+        val uri = "http://10.0.2.2/getfood/usuario"
+        var queue = Volley.newRequestQueue(this)
+        val listener = Response.Listener<JSONObject> { response ->
+            Log.e("Mensaje", response.toString())
+            resp=true
+        }
+        val error = Response.ErrorListener { error ->
+            Log.e("Mensaje", error.message!!)
+        }
+
+        val request = JsonObjectRequest(Request.Method.POST, uri, json, listener, error)
+        queue.add(request)
     }
 }
--- app/src/main/java/mx/tec/getfood/elemento/adapter/CustomAdapter.kt
+++ app/src/main/java/mx/tec/getfood/elemento/adapter/CustomAdapter.kt
@@ -1,25 +1,29 @@
 package mx.tec.getfood.elemento.adapter
 
 import android.content.Context
+import android.graphics.BitmapFactory
+import android.util.Base64
+import android.util.Base64.decode
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.view.animation.AnimationUtils
-import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import mx.tec.getfood.R
 import mx.tec.getfood.elemento.model.Elemento
-import mx.tec.getfood.ui.home.HomeFragment
 
-class CustomAdapter(private val context: Context,
-                    private val layout: Int,
-                    private val dataSource: List<Elemento>,
-                    private val animation:Int)
+
+class CustomAdapter(
+    private val context: Context,
+    private val layout: Int,
+    private val dataSource: List<Elemento>,
+    private val animation: Int
+)
     : RecyclerView.Adapter<CustomAdapter.ElementoViewHolder>() {
 
-    class ElementoViewHolder (inflater: LayoutInflater, parent: ViewGroup, layout: Int)
+    class ElementoViewHolder(inflater: LayoutInflater, parent: ViewGroup, layout: Int)
         : RecyclerView.ViewHolder(inflater.inflate(layout, parent, false)){
         var imagen: ImageView? = null
         var nombre: TextView? = null
@@ -35,7 +39,9 @@ class CustomAdapter(private val context: Context,
         }
 
         fun bindData(elemento: Elemento){
-            imagen!!.setImageResource(elemento.imagen)
+            val imageBytes = decode(elemento.imagen, Base64.DEFAULT)
+            val decodedImage = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
+            imagen!!.setImageBitmap(decodedImage)
             nombre!!.text = elemento.nombre
             descripcion!!.text = elemento.descripcion
             costo!!.text = elemento.costo
